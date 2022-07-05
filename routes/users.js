a955ec1232f430448258ee956d38a8aec3ae19d0
sonu pachauri <engg.cse12@gmail.com>
2022-07-06 01:40:55 +0530

    testing

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/users_controller');
 
 
-router.get('/profile',userController.profile);
+router.get('/profile',userController.profile); 
 
 
 module.exports=router;
